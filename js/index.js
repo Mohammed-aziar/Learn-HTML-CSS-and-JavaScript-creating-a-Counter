76967a1474acaccbec4933924cdcf62afeeaffa7
Mohammed-aziar <mohammed.aziar@gmail.com>
2021-10-03 23:16:59 +0100

    add line in file js de style of value >0 color green and value  < 0 color red and 0 is black

--- js/index.js
+++ js/index.js
@@ -19,5 +19,12 @@ btns.forEach((btn) => {
                num=0;
            }
            value.textContent=num;
+           if(num > 0){
+                value.style.color=   "green";
+           }else if( num <0 ){
+                value.style.color=  "red"
+           }else{
+                value.style.color=   "black"
+           }
    });
 });
